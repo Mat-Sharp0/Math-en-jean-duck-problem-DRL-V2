b4e73871957492108669d88cdc5792a1bffada60
Diplodocus-studio <mateo.hiolle@gmail.com>
2026-02-23 12:28:21 +0100

    Refactor training model to use hyperparameters from config; improve environment setup

--- src/train.py
+++ src/train.py
@@ -9,39 +9,43 @@ from src.env.circle_world import CircleWorldEnv
 from src.env.wrappers.reward import DistanceRewardWrapper
 from src.env.wrappers.observation import RelativeObservationWrapper
 
-def make_env(render_mode=None, reward_scale:float=0.02):
+
+def make_env(render_mode=None, reward_scale: float = 1.0):
     env = CircleWorldEnv(render_mode=render_mode)
     env = DistanceRewardWrapper(env, scale=reward_scale)
     env = RelativeObservationWrapper(env)
     return env
 
 
-def train_model(yaml_config_path:str):
-    
+def train_model(yaml_config_path: str):
+
     with open(yaml_config_path, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-    
-    env = DummyVecEnv([make_env(render_mode=config['render_mode'], reward_scale=config['hyperparameters']['reward_scale']) for _ in range(config['training']['n_envs'])])
-    env = VecNormalize(env, norm_obs=True, norm_reward=True)
 
+    hp = config['hyperparameters']  # raccourci lisible
+
+    env = DummyVecEnv([
+        make_env(render_mode=config['render_mode'], reward_scale=hp.get('reward_scale', 1.0))
+        for _ in range(config['training']['n_envs'])
+    ])
+    env = VecNormalize(env, norm_obs=True, norm_reward=True)
 
     if config['algo'] == 'PPO':
         model = PPO(
             config['policy'],
             env,
-            learning_rate=config['hyperparameters']['learning_rate'],
-            n_steps=config['hyperparameters']['n_steps'],
-            batch_size=config['hyperparameters']['batch_size'],
-            n_epochs=config['hyperparameters']['n_epochs'],
-            gae_lambda=config['hyperparameters']['gae_lambda'],
-            clip_range=config['hyperparameters']['clip_range'],
-            ent_coef=config['hyperparameters']['ent_coef'],
-            gamma=config['hyperparameters']['gamma'],
+            learning_rate=hp['learning_rate'],
+            n_steps=hp['n_steps'],
+            batch_size=hp['batch_size'],
+            n_epochs=hp['n_epochs'],
+            gae_lambda=hp['gae_lambda'],
+            clip_range=hp['clip_range'],
+            ent_coef=hp['ent_coef'],
+            gamma=hp['gamma'],
             verbose=config['log']['verbose'],
             tensorboard_log=config['log']['tensorboard_log'],
             device=config['device'],
         )
-
 
     elif config['algo'] == 'TD3':
         n_actions = env.action_space.shape[0]
@@ -49,33 +53,44 @@ def train_model(yaml_config_path:str):
             mean=np.zeros(n_actions),
             sigma=0.1 * np.ones(n_actions)
         )
-
         model = TD3(
             config['policy'],
             env,
-            learning_rate=config['hyperparameters']['learning_rate'],
-            batch_size=256,
+            learning_rate=hp['learning_rate'],
+            batch_size=hp.get('batch_size', 256),
+            gamma=hp.get('gamma', 0.99),
+            tau=hp.get('tau', 0.005),
             action_noise=action_noise,
-            verbose=1,
+            learning_starts=hp.get('learning_starts', 10_000),
+            buffer_size=hp.get('buffer_size', 500_000),
+            train_freq=hp.get('train_freq', 1),
+            gradient_steps=hp.get('gradient_steps', 1),
+            verbose=config['log']['verbose'],
             tensorboard_log=config['log']['tensorboard_log'],
             device=config['device'],
         )
-
 
     elif config['algo'] == 'SAC':
         model = SAC(
             config['policy'],
             env,
-            learning_rate=config['hyperparameters']['learning_rate'],
-            batch_size=256,
-            verbose=1,
+            learning_rate=hp['learning_rate'],
+            batch_size=hp.get('batch_size', 256),
+            gamma=hp.get('gamma', 0.99),
+            tau=hp.get('tau', 0.005),
+            ent_coef=hp.get('ent_coef', 'auto'),
+            target_entropy=hp.get('target_entropy', 'auto'),
+            learning_starts=hp.get('learning_starts', 10_000),
+            buffer_size=hp.get('buffer_size', 500_000),
+            train_freq=hp.get('train_freq', 1),
+            gradient_steps=hp.get('gradient_steps', 1),
+            verbose=config['log']['verbose'],
             tensorboard_log=config['log']['tensorboard_log'],
             device=config['device'],
         )
 
     else:
         raise ValueError(f"Unsupported algorithm: {config['algo']}")
-
 
     model.learn(
         total_timesteps=config['training']['total_timesteps'],
